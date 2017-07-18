c34cff2a5cc309c59ea7c64896b0f4ec44e779bc
Nilpo <devnilpo@gmail.com>
2017-07-18 03:07:30 -0400

    improving login routine

--- examples/echobot.py
+++ examples/echobot.py
@@ -1,31 +1,29 @@
 # original: https://github.com/carpedm20/LINE/blob/master/examples/echobot.py
 
 from line import LineClient, LineGroup, LineContact
+from curve.ttypes import TalkException
 import os
 
 line_id = os.environ["LINE_CLIENT_ID"]
 line_pass = os.environ["LINE_CLIENT_PASS"]
 
 try:
-   #client = LineClient("ID", "PASSWORD")
-   client = LineClient(line_id, line_pass, com_name="Display Name")
-   #print client.authToken #print authToken before error and use this login again
-   #client = LineClient(authToken="AUTHTOKEN")
-   client = LineClient(authToken=client.authToken)
-except:
-   print "Login Failed"
-   exit()
+    client = LineClient(line_id, line_pass, is_mac=False)
+    client = LineClient(authToken=client.authToken, is_mac=False)
+except TalkException as e:
+    print "Login Failed:", e.reason
+    exit(0)
 
 while True:
-   op_list = []
+    op_list = []
 
-   for op in client.longPoll():
-      op_list.append(op)
+    for op in client.longPoll():
+        op_list.append(op)
 
-   for op in op_list:
-      sender   = op[0]
-      receiver = op[1]
-      message  = op[2]
+    for op in op_list:
+        sender   = op[0]
+        receiver = op[1]
+        message  = op[2]
 
-      msg = message.text
-      sender.sendMessage("[%s] %s" % (sender.name, msg)) #send message back to sender
+        msg = message.text
+        sender.sendMessage("[%s] %s" % (sender.name, msg)) #send message back to sender
